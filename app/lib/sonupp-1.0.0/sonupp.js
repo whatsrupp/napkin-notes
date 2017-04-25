bd352de0fb5e5fb832ecb33d5260eb5f7283838b
Nick Rupp <nickrupp95@gmail.com>
2017-04-25 12:00:24 +0100

    wrote test case for checking page content

--- app/lib/sonupp-1.0.0/sonupp.js
+++ app/lib/sonupp-1.0.0/sonupp.js
@@ -5,3 +5,24 @@ function add(){
 
   document.body.appendChild(element);
 }
+
+
+function textNodesUnder(el){
+  var n, a=[], walk=document.createTreeWalker(el,NodeFilter.SHOW_TEXT,null,false);
+  while(n=walk.nextNode()) a.push(n);
+  return a;
+}
+
+function concatenateTextNodes(textNodeArray){
+  var finalString = ''
+
+  for (i=0; i <textNodeArray.length; i++){
+    finalString += textNodeArray[i].nodeValue
+  }
+  return finalString
+}
+
+function checkStringForText(mainString, soughtString){
+  var test = new RegExp(soughtString)
+  return test.test(mainString);
+}
