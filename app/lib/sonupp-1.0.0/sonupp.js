14e80fdca6bdcf0f899f73a1c0b1c8f6008382cb
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-04-25 11:13:23 +0100

    closes #15 Add check URL function

--- app/lib/sonupp-1.0.0/sonupp.js
+++ app/lib/sonupp-1.0.0/sonupp.js
@@ -5,3 +5,7 @@ function add(){
 
   document.body.appendChild(element);
 }
+
+function checkURL() {
+  return window.location.protocol + "//" + window.location.host + "/" + window.location.pathname;
+}
