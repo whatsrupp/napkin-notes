521dcd5068fc070737c7f26ecbc2dc92c805004a
Nick Rupp <nickrupp95@gmail.com>
2017-04-26 20:30:56 +0100

    add some styling

--- controller/createNote.js
+++ controller/createNote.js
@@ -1,16 +1,20 @@
 (function(exports){
 
   function createNote() {
+    generateAndStoreNote()
+    updateNoteList();
+    resetTextArea();
+  }
 
-    var textarea = document.getElementById('note-input-field');
+  function generateAndStoreNote(){
     var noteText = document.getElementById('note-input-field').value;
     var note = new Note();
     note.createNote(noteText);
     noteList.storeNote(note);
+  }
 
-    // var noteController = new NoteController(noteList);
-    // noteController.appendList();
-    updateNoteList();
+  function resetTextArea (){
+    var textarea = document.getElementById('note-input-field');
     textarea.value = '';
   }
 
