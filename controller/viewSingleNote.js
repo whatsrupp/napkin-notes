d01c3a194aa4554575afc4cd0be1565f96ff477f
Nick Rupp <nickrupp95@gmail.com>
2017-04-27 09:39:11 +0100

    Only show one note at a time

--- controller/viewSingleNote.js
+++ controller/viewSingleNote.js
@@ -3,12 +3,21 @@
   viewSingleNote = function(noteIndex) {
       var noteObject = noteList.notepad[noteIndex]
       var noteString = noteObject.text
-
+      var divId = 'note-view-div'
       var noteViewDiv = document.createElement('div');
+
+      clearDiv(divId)
+      noteViewDiv.setAttribute('id', divId );
+
       var noteTextNode = document.createTextNode(noteString);
       noteViewDiv.appendChild(noteTextNode);
       document.body.appendChild(noteViewDiv)
     };
+
+  clearDiv = function(divId){
+    var div = document.getElementById(divId);
+    if (div) document.body.removeChild(div);
+  }
 
   exports.viewSingleNote = viewSingleNote;
 })(this);
